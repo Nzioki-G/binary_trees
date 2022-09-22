8e86fefb971c8d268d092b9f6410993cf3f7104a
overwatch <nzioki.grace@gmail.com>
2022-09-22 21:56:00 +0300

    node-to-root

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a binary tree
+ * @tree: pointer to root node of the tree to measure
+ *
+ * Return: 0 if tree is NULL else depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return depth;
+}
