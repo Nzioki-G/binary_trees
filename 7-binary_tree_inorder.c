5af64991919d56952598d8aefcd2ed70540749cf
overwatch <nzioki.grace@gmail.com>
2022-09-22 21:48:32 +0300

    post-order traverse

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses binary tree using pre-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: pointer to function to call for each node
+ * Return: nothing
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_inorder(tree->left, print_num);
+	func(tree->n);
+	binary_tree_inorder(tree->right, print_num);
+}
