71373e7ced723860664c471ef24be9f06161ebd2
overwatch <nzioki.grace@gmail.com>
2022-09-22 21:44:39 +0300

    insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a right child node to @parent
+ * @parent: node to insert right child into
+ * @value: value of node to insert
+ *
+ * Return: returns the new node on success, NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	/* check if parent had a right child then reallocate as new's right */
+	if (parent->right)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}
