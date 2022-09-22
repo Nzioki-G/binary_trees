13f7087b210efd86186e7dc4f29cbf1aaf073b6c
overwatch <nzioki.grace@gmail.com>
2022-09-22 23:13:22 +0300

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size_l = 0, size_r = 0, size = 0;
+	size_t size_l = 0, size_r = 0, size;
 
-	if (tree)
-	{
-		size_l = binary_tree_size(tree->left);
-		size_r = binary_tree_size(tree->right);
+	if (!tree)
+		return (0);
 
-		size = size_l + size_r + 1;
-	}
+	size_l = binary_tree_size(tree->left); /* all left nodes */
+	size_r = binary_tree_size(tree->right); /* all right nodes */
+	size = size_l + size_r + 1; /* left + right + root */
+
 	return (size);
 }
