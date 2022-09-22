520cf35d38f4de9bd51ea9d742b103790e21082b
overwatch <nzioki.grace@gmail.com>
2022-09-22 21:42:04 +0300

    create new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node of node to be created
+ * @value: value of the node to be created
+ *
+ * Return: returns the node created on success, NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Binary Trees
+Create a node, insert left, insert right<br>
+Delete a full tree and free: post-order traversal<br>
+Leaf: no children, Root: no parent<br>
+Pre-order Traversal: parent, left, right<br>
+Inorder Traversal: left, parent, right<br>
+Post-order Traversal: left, right, parent<br>
+Height (root to farthest leaf)<br>
+Depth (node to root)<br>
