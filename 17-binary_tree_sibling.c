20d936e33e9da1d9424e3ad6c6841ff2cfbf67bc
overwatch <nzioki.grace@gmail.com>
2022-09-25 20:42:58 +0300

    who's my sis?

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: the node to find sibling of
+ *
+ * Return: sibling or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node == node->parent->right)
+		return (node->parent->left);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (NULL);
+}
