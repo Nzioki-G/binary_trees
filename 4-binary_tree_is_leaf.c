96448c88985e1f17fea3e2b24d5f63d3e61b3be9
olusiekwin <olusiekwin@gmail.com>
2023-11-01 01:27:16 +0300

    4. Is leaf + Betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,10 +5,11 @@
  * @node: Pointer to the node to check
  * Return: 1 if node is a leaf, otherwise 0
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-    
-    return (node->left == NULL && node->right == NULL);
+	if (node == NULL)
+		return (0);
+
+	return (node->left == NULL && node->right == NULL);
 }
