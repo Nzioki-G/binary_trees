22428367218b7ab66a6d53fa87e0f2b8c79bb43b
Grace <nzioki.grace@gmail.com>
2023-11-02 12:05:06 +0300

    insert as left child node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_left - insert as left child
  * @parent: pointer to the node that gains a left child
  * @value: data to store in the new node
- * Return: pointer to the node created 
+ * Return: pointer to the node created
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -13,13 +13,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
+	/* allocate space the size of tree's node */
 	left_child = malloc(sizeof(binary_tree_t));
 	if (!left_child)
 		return (NULL);
 
 	left_child->n = value;
 	left_child->parent = parent;
+	left_child->left = NULL;
 	left_child->right = NULL;
+
+	/* if parent->left, replace and make left_child parent */
 	if (parent->left)
 	{
 		left_child->left = parent->left;
